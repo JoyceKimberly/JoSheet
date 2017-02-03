a48fda0885a2784ef8079f82353a0d4f5c732afb
Jo Kim <joycekimberly666@gmail.com>
2017-02-03 20:45:51 +0100

    Update

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,36 @@
 (function($) { $(document).ready( function() {
 
 var ratio = Math.sqrt(2);
-var vw = $(window).width();
+var vw = document.documentElement.clientWidth;
 var pxWidth = 595;
-var width = /*vw-40;*/ pxWidth;
+var width = /*pxWidth*/ vw-20;
 
-$("#page").width(width).height(width*ratio);
+$("#page").css({
+	width: width,
+    height: width*ratio,
+});
 
-$(".attrBlock").width(54).height(64);
-$(".attrBg").width($(".attrBlock").width()).height($(".attrBlock").height());
-$(".attrTxt").width($(".attrBlock").width());
+$(".attrBlock").css({
+	width: (54*width)/pxWidth,
+    height: (64*width)/pxWidth,
+	left: (22*width)/pxWidth,
+});
+$(".attrBg").css({
+	width: $(".attrBlock").width(),
+    height: $(".attrBlock").height(),
+});
+$(".attrTxt").css({
+	width: $(".attrBlock").width(),
+});
+
+$(".attrBlock.cha").css({
+	top: (484*width)/pxWidth,
+});
+$(".attrTxt").css({
+	top: (6*width)/pxWidth,
+	fontSize: (10*width)/pxWidth,
+});
+
 
 } );
 
