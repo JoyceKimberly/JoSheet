238c55c12a735914642e85638cfbd7220ffae601
Jo Kim <JoyceKimberly@users.noreply.github.com>
2017-02-03 20:54:10 +0100

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -6,29 +6,29 @@ var pxWidth = 595;
 var width = /*pxWidth*/ vw-20;
 
 $("#page").css({
-	width: width,
+    width: width,
     height: width*ratio,
 });
 
 $(".attrBlock").css({
-	width: (54*width)/pxWidth,
+    width: (54*width)/pxWidth,
     height: (64*width)/pxWidth,
-	left: (22*width)/pxWidth,
+    left: (22*width)/pxWidth,
 });
 $(".attrBg").css({
-	width: $(".attrBlock").width(),
+    width: $(".attrBlock").width(),
     height: $(".attrBlock").height(),
 });
 $(".attrTxt").css({
-	width: $(".attrBlock").width(),
+    width: $(".attrBlock").width(),
 });
 
 $(".attrBlock.cha").css({
-	top: (484*width)/pxWidth,
+    top: (484*width)/pxWidth,
 });
 $(".attrTxt").css({
-	top: (6*width)/pxWidth,
-	fontSize: (10*width)/pxWidth,
+    top: (6*width)/pxWidth,
+    fontSize: (10*width)/pxWidth,
 });
 
 
