7eba1988bd8105b6008110dd39a36c8751db23bd
Jo Kim <JoyceKimberly@users.noreply.github.com>
2017-02-03 23:21:04 +0100

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -6,33 +6,33 @@ var width = /* 595 */ vw-20;
 var edge = 0.25*72
 
 $(".container").css({
-    width: width,
-    height: width*ratio,
-    padding: edge,
+    "width": width,
+    "height": width*ratio,
+    "padding": edge,
 });
 $(".page").css({
-    width: width-(edge*2),
-    height: (width*ratio)-(edge*2),
+    "width": width-(edge*2),
+    "height": (width*ratio)-(edge*2),
 });
 
 $(".attrBlock").css({
-    width: (54*width)/pxWidth,
-    height: (64*width)/pxWidth,
-    left: (22*width)/pxWidth,
+    "width": (54*width)/pxWidth,
+    "height": (64*width)/pxWidth,
+    "left": (22*width)/pxWidth,
 });
 $(".attrBg").css({
-    width: $(".attrBlock").width(),
-    height: $(".attrBlock").height(),
+    "width": $(".attrBlock").width(),
+    "height": $(".attrBlock").height(),
 });
 $(".attrTxt").css({
-    width: $(".attrBlock").width(),
+    "width": $(".attrBlock").width(),
 });
 
 $(".attrBlock.cha").css({
-    top: (484*width)/pxWidth,
+    "top": (484*width)/pxWidth,
 });
 $(".attrTxt").css({
-    top: (6*width)/pxWidth,
+    "top": (6*width)/pxWidth,
     "font-size": (10*width)/pxWidth,
 });
 
