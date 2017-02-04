a495da10575930b0e42c70ccffbc8a35fb157b0b
Jo Kim <JoyceKimberly@users.noreply.github.com>
2017-02-04 18:47:50 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,7 +1,6 @@
 (function($) { $(document).ready(function() {
 
     var vWidth = $(".container").outerWidth(true);
-
     $("head").append('<meta name="viewport" content="width=' + vWidth + '">');
 
     interact(".draggable").draggable({
@@ -16,19 +15,19 @@
 }); // ----------------------------------------------------------------------------------
 
 function dragMoveListener (event) {
-	var target = event.target,
+    var target = event.target,
 		// keep the dragged position in the data-x/data-y attributes
 		x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
 		y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-	// translate the element
-	target.style.webkitTransform =
-	target.style.transform =
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
 		'translate(' + x + 'px, ' + y + 'px)';
 
-	// update the posiion attributes
-	target.setAttribute('data-x', x);
-	target.setAttribute('data-y', y);
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
 }
 
 
