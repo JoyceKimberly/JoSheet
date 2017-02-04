da3d7ebfc1ed275a0243ffa03198a8c944c274b0
Jo Kim <JoyceKimberly@users.noreply.github.com>
2017-02-04 18:55:24 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -11,19 +11,27 @@
         onmove: dragMoveListener,
     });
 
+    interact(".attrBlock").draggable({
+        restrict: {
+            restriction: '.attrBox',
+            elementRect: { top: 0, right: 1, bottom: 1, left: 0 },
+        },
+        onmove: dragMoveListener,
+    });
+    
 
 }); // ----------------------------------------------------------------------------------
 
 function dragMoveListener (event) {
     var target = event.target,
-		// keep the dragged position in the data-x/data-y attributes
-		x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-		y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
     // translate the element
     target.style.webkitTransform =
     target.style.transform =
-		'translate(' + x + 'px, ' + y + 'px)';
+        'translate(' + x + 'px, ' + y + 'px)';
 
     // update the posiion attributes
     target.setAttribute('data-x', x);
