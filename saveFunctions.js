c08e3b9559f99782ac8b7c9aeaa9ba4ab13d3052
JoyceKimberly <joyce.beem@gmail.com>
2017-02-09 16:06:36 +0100

    Update

--- saveFunctions.js
+++ saveFunctions.js
@@ -1,13 +1,49 @@
 (function($) { $(document).ready(function() { // ----------------------------------------
 
-var dbx = new Dropbox({ accessToken: 'QLhFo6KEPX8AAAAAAAAS2mpkqK__bG4lzw-8QsXc3-haIZ6jBTTMpxW886F9cxIF' });
-dbx.filesListFolder({path: ''})
-	.then(function(response) {
-	    console.log(response);
-	})
-	.catch(function(error) {
-	    console.log(error);
-	});
+var CLIENT_ID = 'ztucdd8z8fjuh08';
+
+// Parses the url and gets the access token if it is in the urls hash
+function getAccessTokenFromUrl() {
+    return utils.parseQueryString(window.location.hash).access_token;
+}
+// If the user was just redirected from authenticating, the urls hash will
+// contain the access token.
+function isAuthenticated() {
+    return !!getAccessTokenFromUrl();
+}
+// Render a list of items to #files
+function renderItems(items) {
+  var filesContainer = document.getElementById('files');
+  items.forEach(function(item) {
+    var li = document.createElement('li');
+    li.innerHTML = item.name;
+    filesContainer.appendChild(li);
+  });
+}
+// This example keeps both the authenticate and non-authenticated setions
+// in the DOM and uses this function to show/hide the correct section.
+function showPageSection(elementId) {
+  document.getElementById(elementId).style.display = 'block';
+}
+if (isAuthenticated()) {
+  showPageSection('authed-section');
+  // Create an instance of Dropbox with the access token and use it to
+  // fetch and render the files in the users root directory.
+  var dbx = new Dropbox({ accessToken: getAccessTokenFromUrl() });
+  dbx.filesListFolder({path: ''})
+    .then(function(response) {
+      renderItems(response.entries);
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+} else {
+  showPageSection('pre-auth-section');
+  // Set the login anchors href using dbx.getAuthenticationUrl()
+  var dbx = new Dropbox({ clientId: CLIENT_ID });
+  var authUrl = dbx.getAuthenticationUrl('http://localhost:8080/auth');
+  document.getElementById('authlink').href = authUrl;
+}
 
 }); // ----------------------------------------------------------------------------------
 
