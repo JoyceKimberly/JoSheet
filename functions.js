ce969e52b0c86ece6e07840a83d9eef1c6fb8c4a
Jo Kim <JoyceKimberly@users.noreply.github.com>
2017-02-13 20:57:47 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -89,7 +89,7 @@ function moveListener(event) {
 
 var restrictObj = {
     restriction: '.page',
-    endOnly: true,
+    //endOnly: true,
     elementRect: { top: 0, right: 1, bottom: 1, left: 0 },
 };
 
