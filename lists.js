61223a079d01d4f8f9ee39706c8443f9436a6dd0
Jo Kim <JoyceKimberly@users.noreply.github.com>
2017-02-15 11:02:50 +0100

    Update lists.js

--- lists.js
+++ lists.js
@@ -46,7 +46,7 @@ window.displayLevel = function() {
     $('#levelDisplay').text(file.character.level);
 };
 window.displayRace = function() {
-    if ( RaceList[file.character.race].name !== "undefined" ) {
+    if ( typeof RaceList[file.character.race].name !== "undefined" ) {
         $('#raceInput').val(file.character.race);
         $('#raceDisplay').text(RaceList[file.character.race].name);
     } else {
