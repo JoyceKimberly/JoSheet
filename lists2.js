ea708ba32cf8b5f604975e8cf77136ee562c671e
JoyceKimberly <joyce.beem@gmail.com>
2017-02-20 20:11:03 +0100

    Update

--- lists2.js
+++
@@ -1,6 +0,0 @@
-//Object.prototype.toSource || (Object.prototype.toSource = function() { return this; });
-//ClassSubList.prototype.toSource || (ClassSubList.prototype.toSource = function() { return this; });
-/*Object.prototype.toSource || (Object.prototype.toSource = function() {
-	return JSON.stringify(this);
-});
-*/
