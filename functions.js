27d6ea91b856f5b95338e2d162c89929e15a9d08
Jo Kim <JoyceKimberly@users.noreply.github.com>
2017-02-22 01:25:51 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -19,9 +19,9 @@ var vw = window.innerWidth && document.documentElement.clientWidth ? Math.min(wi
 var zoomScale = ((vw-30)/cWidth);
 
 window.addEventListener("resize", function() {
-    $('#blockMenuContainer').css("margin-top", ($('#navbar').outerHeight()));
+    $('#page1').css("margin-top", ($('#navbar').outerHeight()));
 }, false);
-$('#blockMenuContainer').css("margin-top", ($('#navbar').outerHeight()));
+$('#page1').css("margin-top", ($('#navbar').outerHeight()));
 
 if ( !!getAccessToken() ) {
     $('#authLink').hide();
