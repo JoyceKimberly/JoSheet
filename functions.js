551c255a83e1ed6d0ab4dac44787a12f3f344daa
Jo Kim <JoyceKimberly@users.noreply.github.com>
2017-02-28 21:47:45 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -431,7 +431,7 @@ var characterFiles = [];
     };
   };
 
-  console.log(ClassList); /*debug*/
+  console.log(tDoc); /*debug*/
   
   // ------------------------------------------------------------------------------------
   // -- Dropbox --
