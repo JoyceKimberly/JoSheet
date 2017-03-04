eedc116837cd7f2c9aa337cd261d21c2d5034031
Jo Kim <JoyceKimberly@users.noreply.github.com>
2017-03-04 15:07:28 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -278,8 +278,7 @@ var characterFiles = [];
 
     } else if ( dit.is('.number') ) {
       character[key] = Number(dit.val());
-      console.log(event);
-      event.CalcSkill();
+      console.log(event.CalcSkill());
 
     } else if ( dit.is('div') ) {
       character[key] = dit.text();
