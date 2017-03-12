ef930772f6f54b731a2f231bc76173ca725aca8e
Jo Kim <JoyceKimberly@users.noreply.github.com>
2017-03-12 20:42:43 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -419,11 +419,11 @@ $(function() { // --------------------------------------------------------------
     $menu = $('#idealsConfig');
     $.each(Menus.background[1].oSubMenu, function(i, value) {    
       $menu.append('\
-  <label class="custom-control custom-radio">\
-    <input data-index="' + i + '" type="radio" class="custom-control-input">\
-    <span class="custom-control-indicator"></span>\
-    <span class="custom-control-description form-control-sm">' + value.cName + '</span>\
-  </label>\
+<label class="custom-control custom-radio">\
+  <input data-index="' + i + '" name="idealsConfig" type="radio" class="custom-control-input">\
+  <span class="custom-control-indicator"></span>\
+  <span class="custom-control-description form-control-sm">' + value.cName + '</span>\
+</label>\
       ');
     });
     console.log(Menus);
